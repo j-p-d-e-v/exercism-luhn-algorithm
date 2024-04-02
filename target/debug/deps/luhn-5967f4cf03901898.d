dc97cec027248d3f8a2490ee45664d7246af9940
JP <jpmateo@JPMAC.local>
2024-04-02 16:00:50 +0800

    Initial Commit

--- target/debug/deps/luhn-5967f4cf03901898.d
+++
@@ -1,5 +0,0 @@
-/Users/jpmateo/Codes/rust/exercism/luhn/target/debug/deps/luhn-5967f4cf03901898: src/lib.rs
-
-/Users/jpmateo/Codes/rust/exercism/luhn/target/debug/deps/luhn-5967f4cf03901898.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/luhn-94dc555052bb70d1.d
+++
@@ -1,5 +0,0 @@
-/Users/jpmateo/Codes/rust/exercism/luhn/target/debug/deps/luhn-94dc555052bb70d1: src/main.rs
-
-/Users/jpmateo/Codes/rust/exercism/luhn/target/debug/deps/luhn-94dc555052bb70d1.d: src/main.rs
-
-src/main.rs:
